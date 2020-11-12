0091f1cd4e5ad466d5a44b1619fdcca3f46eeceb
peter <948430126@qq.com>
2020-11-12 15:20:15 +0800

    创建<Layout/>

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,104 +1,8 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-      <p>Lables.vue</p>
-    </div>
-    <Nav/>
+  <div>
+  <layout>
+    <p>Lables.vue</p>
+  </layout>
   </div>
 </template>
 
@@ -107,17 +11,3 @@ export default {
 name: "Labels"
 }
 </script>
-
-<style lang="scss" scoped>
-.nav-wrapper{
-  border:1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content{
-  border:1px solid blue;
-  overflow:auto;
-  flex-grow: 1;
-}
-</style>
